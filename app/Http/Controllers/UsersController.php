bbeb3a0d7e0e0b7529c7d7e63855059fae81943e
JonathanLeal <jonathan.e.leal.m@gmail.com>
2023-05-07 13:06:18 -0600

    agregando endpoint register

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\Http;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
-    public function register(){
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return http::respuesta(http::retError, $validator->errors());
+        }
+
+        $users = new User();
+        $users->name = $request->name;
+        $users->email = $request->email;
+        $users->password = Hash::make($request->password);
+        $users->save();
         return Http::respuesta(http::retOK, "funcionando");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/register', [UsersController::class, 'register']);
+Route::post('/register', [UsersController::class, 'register']);
